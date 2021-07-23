a1f2dedb493c7c754a7d942276f19aacfad9c4bb
znol1 <57703588+znol1@users.noreply.github.com>
2021-07-23 23:42:25 +0300

    ви срфтпуы

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -27,107 +27,147 @@ class _DetailsScreenState extends State<DetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(user.email),
+        title: Text(user.username),
       ),
-      body: ListView(
-        shrinkWrap: true,
-        children: [
-          Text(user.name),
-          Text(user.email),
-          Text(user.phone.toString()),
-          Text(user.website),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('working'),
-              Text(user.working.name),
-              Text(user.working.bs),
-              Text(user.working.catchPhrase),
-            ],
-          ),
-          Text('address ${user.address}'),
-          GestureDetector(
-            onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => PostsScreen(user))),
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-              ),
-              child: user.posts.length >= 3
-                  ? Column(
-                      children: [
-                        ListTile(
-                          title: Text(user.posts[0].title),
-                          subtitle: Text(
-                              '${user.posts[0].text.substring(1, 50)} ...'),
-                        ),
-                        ListTile(
-                          title: Text(user.posts[1].title),
-                          subtitle: Text(
-                              '${user.posts[1].text.substring(1, 50)} ...'),
-                        ),
-                        ListTile(
-                          title: Text(user.posts[2].title),
-                          subtitle: Text(
-                              '${user.posts[2].text.substring(1, 50)} ...'),
-                        ),
-                      ],
-                    )
-                  : Text('Здесь будут появляться ваши посты'),
-            ),
-          ),
-          GestureDetector(
-            onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => AlbumsScreen(user.albums))),
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-              ),
-              child: Row(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Column(
-                    children: [
-                      Container(
-                          width: 100,
-                          child: Image(
-                              image:
-                                  NetworkImage(user.albums[0].photos[0].link))),
-                      Text(user.albums[0].description),
-                    ],
-                  )
+                  Text('Имя: ${user.name}'),
+                  Text('Email: ${user.email}'),
+                  Text('Номер: ${user.phone.toString()}'),
+                  Text(user.website),
                 ],
               ),
             ),
-          ),
-        ],
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Работа',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(user.working.name),
+                  Text(user.working.bs),
+                  Text(user.working.catchPhrase),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              child: Text('address ${user.address}'),
+            ),
+            GestureDetector(
+              onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => PostsScreen(user))),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 3,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: user.posts.length >= 3
+                    ? ListView.builder(
+                        itemCount: 3,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(user.posts[index].title),
+                            subtitle: Text(
+                                '${user.posts[index].text.substring(1, 50)} ...'),
+                          );
+                        })
+                    : ListView.builder(
+                        itemCount: user.posts.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(user.posts[index].title),
+                            subtitle: Text(
+                                '${user.posts[index].text.substring(1, 50)} ...'),
+                          );
+                        }),
+              ),
+            ),
+            GestureDetector(
+              onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => AlbumsScreen(user.albums))),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 5,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: user.albums.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 120,
+                      height: MediaQuery.of(context).size.height / 5,
+                      child: Column(
+                        children: [
+                          Image(
+                              image: NetworkImage(
+                                  user.albums[index].photos[0].link)),
+                          Text(user.albums[0].description),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
